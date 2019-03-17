64d99445b7213602c188f427c99d3931854e1cb3
Gustav Ljungberg <g.ljungberg@live.se>
2019-03-17 07:42:27 +0100

    added method doSomething, added META-INF

--- src/main/java/PluginMain.java
+++ src/main/java/PluginMain.java
@@ -1,6 +1,19 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import http.server.response.HttpResponse;
 import http.server.serviceloader.PluginType;
 
 @PluginType("/v1/plugin")
 public class PluginMain {
 
+    public HttpResponse doSomething(String doThis, Map<String, List<String>> params) {
+		
+    	
+      
+    	
+    	
+        return new HttpResponse();
+    }
 }
