1f18c39e6f8ba47495845c827825d442161f6181
Gustav Ljungberg <g.ljungberg@live.se>
2019-03-17 08:46:31 +0100

    new class Calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,25 @@
+
+public class Calculator {
+
+	protected Double add(double num, double num2) {
+		return (double)num+num2;
+	}
+	
+	protected Double subtract(double num, double num2) {
+		return (double)num-num2;
+	}
+	
+	protected Double multiply(double num, double num2 ) {
+		return num*num2;
+	}
+	
+	protected Double divide(double num, double num2) {
+		return num/num2;
+	}
+	
+	protected Double square(double num) {
+		return num*num;
+	}
+	
+	
+}
--- src/main/java/PluginMain.java
+++ src/main/java/PluginMain.java
@@ -5,15 +5,33 @@ import java.util.Map.Entry;
 import http.server.response.HttpResponse;
 import http.server.serviceloader.PluginType;
 
-@PluginType("/v1/plugin")
+@PluginType("/v1/calculator")
 public class PluginMain {
-
+	
+	private static String[] tasks = {"add", "sub", "mult", "div", "sqr"};
+	Calculator calculator = new Calculator();
     public HttpResponse doSomething(String doThis, Map<String, List<String>> params) {
-		
     	
-      
+    	String task = "";
+    	switch(task) {
+    	
+    		case "add":
+    			return new HttpResponse(calculator.add(1,1).toString());
+    		case "sub":
+    			return new HttpResponse(calculator.subtract(1,1).toString());
+    		case "mult":
+    			return new HttpResponse(calculator.multiply(1,1).toString());
+    		case "div":
+    			return new HttpResponse(calculator.divide(1,1).toString());
+    		case "sqr":
+    			return new HttpResponse(calculator.square(1).toString());
+    		default:
+    			return new HttpResponse("Invalid task");
+    			
+    	}
     	
     	
-        return new HttpResponse();
+    	
+    	
     }
 }
